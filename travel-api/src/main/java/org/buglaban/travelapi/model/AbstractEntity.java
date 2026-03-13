e5fa07a6f4da0d9243e2dbe84d85522759606f7d
banvt05 <trinhvanban532004@gmail.com>
2026-03-13 08:58:42 +0700

    user

---
+++ travel-api/src/main/java/org/buglaban/travelapi/model/AbstractEntity.java
@@ -0,0 +1,33 @@
+package org.buglaban.travelapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public abstract class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @CreationTimestamp //auto insert time vao DB
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date created_at;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updated_at;
+}
